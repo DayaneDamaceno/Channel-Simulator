6dd5cadc60b8fc9ab0c5b0af6d491c84047ed7f9
Dayane Damaceno <Dayane.Damaceno@t-systems.com>
2023-10-22 15:56:37 -0300

    Estrutura inicial

---
+++ src/main/java/com/fesa/channelsimulator/ChannelsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.fesa.channelsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelsimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fesa/channelsimulator/ChannelsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fesa.channelsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e91f28284022acbbddab585fea6542b5c6f58246
Duarte <duartebpires2@gmail.com>
2023-11-05 10:10:16 -0300

    6 Graficos prontos.

--- src/main/resources/static/js/graphics.js
+++ src/main/resources/static/js/graphics.js
@@ -1,39 +1,91 @@
 
 function iniciarGraficoSinalEntrada(){
-    let exp = "Math.sin(x)";
+
+    let f1 = parseFloat(document.querySelector("#f1").value);
+
+    let exp = "Math.abs(Math.sin(2*Math.PI*" + f1 + "*x))";
 
     // Generate values
     const xValues = [];
     const yValues = [];
-    for (let x = 0; x <= 10; x += 0.1) {
+    for (let x = 0; x <= 100; x += 0.01) {
         xValues.push(x);
         yValues.push(eval(exp));
     }
 
     // Display using Plotly
     const data = [{x:xValues, y:yValues, mode:"lines"}];
-    const layout = { title: "Sinal de entrada",};
-    Plotly.newPlot("graficoSinalEntrada", data, layout);
+    const layout = { title: "Sinal de Entrada",};
+    Plotly.newPlot("graficoSinalEntrada", data, layout, {displayModeBar:false, scrollZoom: true});
+
+    autoEscala("graficoSinalEntrada");
 }
 
 function iniciarGraficoAmplitudeEntrada(){
-    const xArray1 = [10,10];
-    const yArray1 = [10,0];
 
-    const xArray2 = [20,20];
-    const yArray2 = [20,0];
+    let f1 = parseFloat(document.querySelector("#f1").value);
+
+    const xArray1 = [0,0];
+    const yArray1 = [0.63661,0];
+
+    const xArray2 = [1*f1,1*f1];
+    const yArray2 = [0,0.2122];
+
+    const xArray3 = [2*f1,2*f1];
+    const yArray3 = [0,0.04244];
+
+    const xArray4 = [3*f1,3*f1];
+    const yArray4 = [0,0.01818];
+
+    const xArray5 = [4*f1,4*f1];
+    const yArray5 = [0,0.01010];
+
+    const xArray6 = [5*f1,5*f1];
+    const yArray6 = [0,0.00643];
 
     // Define Data
     const data = [
         {
             x: xArray1,
             y: yArray1,
+            name:"2/π",
             mode:"lines"
+
         },
         {
             x: xArray2,
             y: yArray2,
+            name:"-2/3π",
             mode:"lines"
+
+        },
+        {
+            x: xArray3,
+            y: yArray3,
+            name:"-2/15π",
+            mode:"lines"
+
+        },
+        {
+            x: xArray4,
+            y: yArray4,
+            name:"-2/35π",
+            mode:"lines"
+
+        },
+        {
+            x: xArray5,
+            y: yArray5,
+            name:"-2/63π",
+            mode:"lines"
+
+        },
+        {
+            x: xArray6,
+            y: yArray6,
+            name:"-2/99π",
+            mode:"lines"
+
         }
     ];
 
@@ -41,31 +93,26 @@ function iniciarGraficoAmplitudeEntrada(){
     const layout = {
         xaxis: {range: [0, 160], title: ""},
         yaxis: {range: [0, 16], title: ""},
-        title: "Amplitude de entrada",
+        title: "Amplitude de Entrada",
 
     };
 
     // Display using Plotly
-    Plotly.newPlot("graficoAmplitudeEntrada", data, layout);
+    Plotly.newPlot("graficoAmplitudeEntrada", data, layout, {displayModeBar:false, scrollZoom: true, responsive: true});
+
+    autoEscala("graficoAmplitudeEntrada");
 }
 
 function iniciarGraficoFaseEntrada(){
-    const xArray1 = [10,10];
-    const yArray1 = [10,0];
-
-    const xArray2 = [20,20];
-    const yArray2 = [20,0];
+    const xArray1 = [0,50];
+    const yArray1 = [0,0];
 
     // Define Data
     const data = [
         {
             x: xArray1,
             y: yArray1,
-            mode:"lines"
-        },
-        {
-            x: xArray2,
-            y: yArray2,
+            name:"-arctan(bn/an)",
             mode:"lines"
         }
     ];
@@ -74,16 +121,44 @@ function iniciarGraficoFaseEntrada(){
     const layout = {
         xaxis: {range: [0, 160], title: ""},
         yaxis: {range: [0, 16], title: ""},
-        title: "Fase de entrada",
+        title: "Fase de Entrada",
 
     };
 
     // Display using Plotly
-    Plotly.newPlot("graficoFaseEntrada", data, layout);
+    Plotly.newPlot("graficoFaseEntrada", data, layout, {displayModeBar:false, scrollZoom: true});
+
+    autoEscala("graficoFaseEntrada");
 }
 
-function iniciarGraficoCanal(){
-    let exp = "Math.sin(x)";
+function iniciarGraficoAmpliCanal(){
+
+    let f2 = parseFloat(document.querySelector("#f2").value);
+    let f3 = parseFloat(document.querySelector("#f3").value);
+
+    let exp = "(1/ "+ f2 +")*(x/Math.sqrt((1+(Math.pow((x/"+ f2 +"),2)))*(1+(Math.pow((x/"+ f3 +"),2)))))";
+
+    // Generate values
+    const xValues = [];
+    const yValues = [];
+    for (let x = 0; x <= 100; x += 0.1) {
+        xValues.push(x);
+        yValues.push(eval(exp));
+    }
+
+    // Display using Plotly
+    const data = [{x:xValues, y:yValues, mode:"lines"}];
+    const layout = { title: "Amplitude do Canal",};
+    Plotly.newPlot("graficoAmpliCanal", data, layout, {displayModeBar:false, scrollZoom: true});
+
+    autoEscala("graficoAmpliCanal");
+}
+
+function iniciarGraficoFaseCanal(){
+    let f2 = parseFloat(document.querySelector("#f2").value);
+    let f3 = parseFloat(document.querySelector("#f3").value);
+
+    let exp = "((-Math.PI/2) - Math.atan((x*("+ (f2 + f3) +"))/( "+ (f2*f3) +" - Math.pow(x,2))))*(180/Math.PI)";
 
     // Generate values
     const xValues = [];
@@ -95,25 +170,38 @@ function iniciarGraficoCanal(){
 
     // Display using Plotly
     const data = [{x:xValues, y:yValues, mode:"lines"}];
-    const layout = { title: "Canal Passa Faixa",};
-    Plotly.newPlot("graficoCanal", data, layout);
+    const layout = { title: "Fase do Canal",};
+    Plotly.newPlot("graficoFaseCanal", data, layout, {displayModeBar:false, scrollZoom: true});
+
+    autoEscala("graficoFaseCanal");
 }
 
 function iniciarGraficoSinalSaida(){
-    let exp = "Math.sin(x)";
+
+    let f1 = parseFloat(document.querySelector("#f1").value);
+    let f2 = parseFloat(document.querySelector("#f2").value);
+    let f3 = parseFloat(document.querySelector("#f3").value);
+
+    var funcG = (1/f2)*(f1/Math.sqrt((1+(Math.pow((f1/f2),2)))*(1+(Math.pow((f1/f3),2)))));
+    var funcFase = ((-Math.PI/2) - Math.atan((f1*(f2 + f3))/((f2*f3)- Math.pow(f1,2))))*(180/Math.PI);
+
+    let exp = funcG + "*Math.abs(Math.sin(2*Math.PI*" + f1 + "* x +"+ funcFase +"))";
+    console.log(exp);
 
     // Generate values
     const xValues = [];
     const yValues = [];
-    for (let x = 0; x <= 10; x += 0.1) {
+    for (let x = 0; x <= 25; x += 0.001) {
         xValues.push(x);
         yValues.push(eval(exp));
     }
 
     // Display using Plotly
     const data = [{x:xValues, y:yValues, mode:"lines"}];
-    const layout = { title: "Sinal de Saida",};
-    Plotly.newPlot("graficoSinalSaida", data, layout);
+    const layout = { title: "Sinal de Saída",};
+    Plotly.newPlot("graficoSinalSaida", data, layout, {displayModeBar:false, scrollZoom: true});
+
+    autoEscala("graficoSinalSaida");
 }
 
 function iniciarGraficoAmplitudeSaida(){
@@ -146,7 +234,7 @@ function iniciarGraficoAmplitudeSaida(){
     };
 
     // Display using Plotly
-    Plotly.newPlot("graficoAmplitudeSaida", data, layout);
+    Plotly.newPlot("graficoAmplitudeSaida", data, layout, {displayModeBar:false, scrollZoom: true});
 }
 
 function iniciarGraficoFaseSaida(){
@@ -174,19 +262,39 @@ function iniciarGraficoFaseSaida(){
     const layout = {
         xaxis: {range: [0, 160], title: ""},
         yaxis: {range: [0, 16], title: ""},
-        title: "Fase de Saida",
+        title: "Fase de Saída",
 
     };
 
     // Display using Plotly
-    Plotly.newPlot("graficoFaseSaida", data, layout);
+    Plotly.newPlot("graficoFaseSaida", data, layout, {displayModeBar:false, scrollZoom: true});
 }
 
 iniciarGraficoSinalEntrada();
 iniciarGraficoAmplitudeEntrada();
 iniciarGraficoFaseEntrada();
-iniciarGraficoCanal();
+iniciarGraficoAmpliCanal();
+iniciarGraficoFaseCanal();
 iniciarGraficoSinalSaida();
 iniciarGraficoAmplitudeSaida();
 iniciarGraficoFaseSaida();
 
+function atualizarSinalGrafico() {
+    setTimeout(function(){
+        iniciarGraficoSinalEntrada();
+        iniciarGraficoAmplitudeEntrada();
+        iniciarGraficoFaseEntrada();
+        iniciarGraficoAmpliCanal();
+        iniciarGraficoFaseCanal();
+        iniciarGraficoSinalSaida();
+        iniciarGraficoAmplitudeSaida();
+        iniciarGraficoFaseSaida();
+    }, 200);
+}
+
+function autoEscala(grafico) {
+    Plotly.relayout(grafico, {
+        'xaxis.autorange': true,
+        'yaxis.autorange': true
+    });
+}
